014c2f88c82b53ac09f96555173bcf099f689665
Giles Knap <giles.knap@diamond.ac.uk>
2022-05-18 15:11:55 +0100

    add run script

---
+++ run_test.sh
@@ -0,0 +1,24 @@
+echo '------------------ Expected output: 
+
+# Source: app-test/templates/ioc.yaml
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name:  release-name
+  namespace: bl45p
+  labels:
+    app: release-name
+    beamline: BeamlineNameFromApplicationValues
+    ioc_version: "2021.2.0"
+    is_ioc: "True"
+data:
+    ---- Start of Values.data
+    This Data is from the Library Chart values.yaml
+    ----- End of Values.data
+
+
+
+------------------ Actual output: 
+'
+helm dependency build app
+helm template app
